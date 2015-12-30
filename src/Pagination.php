451318bf7d2a034ae5a32316963f1700c02e7112
Sam Williams <samuel.williams8@gmail.com>
2015-12-30 11:37:07 +1100

    Add license statement

--- src/Pagination.php
+++ src/Pagination.php
@@ -1,5 +1,13 @@
 <?php
-
+/*
+ * This file is part of Simple Pagination.
+ *
+ * (c) Samuel Williams <sam@badcow.co>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+ 
 class Pagination
 {
     /**
